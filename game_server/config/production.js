fcd146c33259fd337959115429c9748b36a089ec
Adebayour66265 <adebayour66265@gmail.com>
2025-06-26 13:42:33 +0100

    update

--- game_server/config/production.js
+++ game_server/config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
 	// dbconnection:'mongodb://poenghskcdj:2sEHdke3eDi83K0ds@172.232.73.254:34256/peijdnrnsajr',
-	dbconnection:'mongodb://poenghskcdj:2sEHdke3eDi83K0ds@13.233.159.124:34256/peijdnrnsajr',
+	dbconnection:'mongodb+srv://resume:1234@cluster0.8114dlp.mongodb.net/bc-clone?retryWrites=true&w=majority',
 
 }
