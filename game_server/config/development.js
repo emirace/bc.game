eb12b52a0c0476886da9c764873216516010bcb1
emirace <emmanuelakwuba57@gmail.com>
2025-07-02 22:22:02 +0100

    add development configuration files for game server

---
+++ game_server/config/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // dbconnection:'mongodb://poenghskcdj:2sEHdke3eDi83K0ds@172.232.73.254:34256/peijdnrnsajr',
+  dbconnection:
+    "mongodb+srv://emmanuelakwuba57:cfaxhgu5GSVBkAtu@emirace.zci1zhz.mongodb.net/bc-clone?retryWrites=true&w=majority&appName=Emirace",
+};
