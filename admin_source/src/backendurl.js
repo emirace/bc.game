0a456f0a8e7e83872e471a21945345095bb9cd2e
emirace <emmanuelakwuba57@gmail.com>
2025-07-02 22:52:17 +0100

    refactor: standardize backend URL assignment for local and production environments

--- admin_source/src/backendurl.js
+++ admin_source/src/backendurl.js
@@ -1,10 +1,10 @@
 var host = document.location.hostname;
-if(host == "localhost" || host == "192.168.1.110") {
-	BackendUrl = 'http://' + document.location.hostname + ':29188/';
-	// BackendUrl = 'https://oeutrystdsnd.rollgame.io/';
+if (host == "localhost" || host == "192.168.1.110") {
+  BackendUrl = "http://" + document.location.hostname + ":29188/";
+  // BackendUrl = 'https://oeutrystdsnd.rollgame.io/';
 } else {
-	BackendUrl = 'https://oeutrystdsnd.rollgame.io/';
+  BackendUrl = "https://bc-game-1-4orr.onrender.com/";
 }
-ApiUrl = 'http://'+document.location.hostname+':2083/';
+ApiUrl = "http://" + document.location.hostname + ":2083/";
 export var BackendUrl;
 export var ApiUrl;
--- game/src/backendurl.js
+++ game/src/backendurl.js
@@ -1,8 +1,8 @@
 var host = document.location.hostname;
-if(host == "localhost" || host == '192.168.1.110') {
-	BackendUrl = 'http://' + document.location.hostname + ':13578/';
-	// BackendUrl = 'https://api.rollgame.io/';
+if (host == "localhost" || host == "192.168.1.110") {
+  BackendUrl = "http://" + document.location.hostname + ":13578/";
+  // BackendUrl = 'https://api.rollgame.io/';
 } else {
-	BackendUrl = 'https://api.rollgame.io/';
+  BackendUrl = "https://bc-game-y4uo.onrender.com/";
 }
 export var BackendUrl;
